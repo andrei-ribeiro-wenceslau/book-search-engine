3f868289162de26ed65f9908b4e68e9c08429c71
Andrei <andrei.ribeirow@gmail.com>
2023-10-16 22:51:32 +1100

    Implementing the Apollo Server and applying it to the Express server as middleware.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+
+// const routes = require('./routes');
+
+const { typeDefs, resolvers } = require("./graphql");
+const db = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+  await server.start();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(routes);
+  app.use("/graphql", expressMiddleware(server));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
+
+  // app.use(routes);
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
