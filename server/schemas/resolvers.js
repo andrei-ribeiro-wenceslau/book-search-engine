3af05f3b4097682c7372128e7a588430cd5faedd
Andrei <andrei.ribeirow@gmail.com>
2023-10-17 21:54:39 +1100

    Defining the necessary query and mutation types. Defining the query functionality in the resolver.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express')
+const { User, Book } = require('../models')
+const { signToken } = require('../utils/auth');
+const { sign } = require('jsonwebtoken');
+
+const resolvers = {
+
+    Query: {
+       
+        me: async (parent, args, context) => {
+            if (context.user) {
+
+                return User.findOne({ _id: context.user._id })
+            }
+            throw new AuthenticationError('You must be logged in to access this information.')
+        }
+    }
+
+}
