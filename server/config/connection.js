79c908abece4a7bd683d95410dd4fc13d30de0ed
Andrei <andrei.ribeirow@gmail.com>
2023-10-19 18:22:03 +1100

    .env config mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+console.log(process.env)
+console.log(process.env.MONGODB_URI) // remove this after you've confirmed it is working
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
